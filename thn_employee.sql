d7729349a6dd7e60881b327cf11314b3a2580a5b
Millie99-thn <thethtetnyein9999@gmail.com>
2025-05-28 11:30:25 +0630

    Create thn_employee.sql

---
+++ thn_employee.sql
@@ -0,0 +1,7 @@
+CREATE TABLE thn_employee (
+    emp_id        NUMBER PRIMARY KEY,
+    name          VARCHAR2(100),
+    position      VARCHAR2(50),
+    salary        NUMBER,
+    updated_at    DATE
+);
